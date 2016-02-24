e3e1503e4c58bf64873cc90fef18bdc048621c46
Sahit M <mathgeek404>
2016-02-23 18:17:18 -0600

    HW4, codes progres

--- HW4/local_sqp.m
+++ HW4/local_sqp.m
@@ -3,21 +3,19 @@ function [] =  local_sqp(f,c,x,lam, initial_x, initial_lam)
 	grad_f = gradient(f)
 	A = jacobian(c,x)
 
-	F = [gradient(f,x)-A'*lam, c(x)]
-	Fj = jacobian(F)
+	F = [gradient(f,x)-A'*lam; c];
+	Fj = jacobian(F,[x;lam])
 	
-	xk = initial_x
-	lk = initial_lam
+	xk = initial_x;
+	lk = [0;0;0]
 	
-	while (true):
-		Fk = subs(Fj,[xk,lk])
+	while 1==1
+		Fk = subs(Fj,[xk;lk])
 		grad_fk = subs(grad_f,xk)
 		ck = subs(c,xk)
 		sol = inv(Fk)*-[Fk, ck]
-		xk = xk + sol[1:5,1]
-		lk = lk + sol[6:8,1]
-		print(xk)
-		print(lk)
+		xk = xk + sol(1:5,1)
+		lk = lk + sol(6:8,1)
 	end
 	
 end
--- HW4/main_sqp.m
+++ HW4/main_sqp.m
@@ -8,9 +8,27 @@ x_initial = [-1.71, 1.59, 1.82, -0.763, -0.763]'
 l_initial = [-1.71, 1.59, 1.82, -0.763, -0.763]'
 
 
-obj = exp(x(1,1)*x(2,1)*x(3,1)*x(4,1)*x(5,1))-(1/2)*(x(1,1)^3+x(2,1)^2+1)^2
+f = exp(x(1,1)*x(2,1)*x(3,1)*x(4,1)*x(5,1))-(1/2)*(x(1,1)^3+x(2,1)^2+1)^2
 c1 = x'*x-10
 c2 = x(2,1)*x(3,1)-5*x(4,1)*x(5,1)
 c3 = x(1,1)^3+x(2,1)^3+1
+c = [c1;c2;c3]
 
-local_sqp(obj,[c1,c2,c3],x,l,x_initial, l_initial)
+%Algo 18.1
+grad_f = gradient(f)
+A = jacobian(c,x)
+
+F = [gradient(f,x)-A'*l; c];
+Fj = jacobian(F,[x;l])
+
+xk = x_initial
+lk = [0;0;0]
+
+while 1==1
+  Fk = subs(Fj,[x;l],[xk;lk])
+  grad_fk = subs(grad_f,x,xk)
+  ck = subs(c,x,xk)
+  sol = inv(Fk)*-[Fk, ck]
+  xk = xk + sol(1:5,1)
+  lk = lk + sol(6:8,1)
+end
