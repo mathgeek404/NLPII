03184b75dc0978a79e32c9393015c17c19563bda
Steveoooo <mathgeek404@gmail.com>
2016-03-01 15:15:48 -0600

    HW5, some code progress

--- HW5/hw5_p1.m
+++ HW5/hw5_p1.m
@@ -1,28 +1,26 @@
 % Homework 4
 % Problem 4
-answer = [-1.71, 1.59, 1.82, -0.763, -0.763]'
+answer = [-1, -1]'
 
 %Variables
 x = sym('x',[5,1])
 l = sym('l',[3,1]) %num constraints
 
 %Initial choices
-x_initial = [-1.8, 1.7, 1.9, -0.8, -0.8]'
-l_initial = [1,-1,1]'
+x_initial = [0,0]'
+l_initial = [0]
 
 f = x(1,1) + x(2,1)
 c1 = x(1,1)^2+x(2,1)^2+2
 
-% µk = 1, 10, 100, 1000 
-
-
-%Given µ0 > 0, a nonnegative sequence {τk } with τk → 0, and a starting point xs
+% µk = 1, 10, 100, 1000
+%Given u0 > 0, a nonnegative sequence {tk } with tk → 0, and a starting point xs
 %for k  0, 1, 2,...
-%Find an approximate minimizer xk of Q(·;µk ), starting at xs and terminating when ;
+%Find an approximate minimizer xk of Q(·;uk ), starting at xs and terminating when ;
 %if final convergence test satisfied
 %stop with approximate solution xk ;
 %end (if)
-%Choose new penalty parameter µk+1 > µk ;
+%Choose new penalty parameter uk+1 > uk ;
 %Choose new starting point xs, k+1;
 %end (for)
 
@@ -34,7 +32,7 @@ array_mu = [1, 10, 100, 1000]
 for idx = 1:numel(array_mu)
     mu_k = array_mu(idx)
     tolerance = 1/mu_k
-    L = f -lk'*c+ (mu_k/2)*(c)^2
+    L = f -lk'*c1+ (mu_k/2)*(c1)^2
     grad_L = gradient(L,x)
     grad_Lk = double(subs(grad_L,x,xk));
     hess_L = hessian(L,x)
